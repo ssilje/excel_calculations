10e0816ec565728e6c0389787f1d8e707c257272
Silje Sørland <siljesor@gmail.com>
2023-03-02 09:37:20 +0100

    Create lokalklima.py

---
+++ lokalklima.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+datadir = '/Users/ssilje/Sweco/lokalklima/NOR_HO_Bergen-Florida.013170_TMYx.2007-2021'
+file = 'NOR_HO_Bergen-Florida.013170_TMYx.2007-2021.clm'
+
+fileread = pd.read_csv(file)
